ba8df76887d89103f5a93fa4364348670bf40adc
Jinseo Sung <jinseosung15@gmail.com>
2023-07-23 13:02:34 +0200

    Create header section

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./utils/styles/style.css";
 import Home from "./pages/Home/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
