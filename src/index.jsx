f25d80792367cd1bac93c2e439847b7eea1bf4c0
Jinseo Sung <jinseosung15@gmail.com>
2023-07-31 17:21:04 +0200

    create ScrollToTop hook

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,13 @@ import Header from "./components/header";
 import Home from "./pages/Home/index";
 import Footer from "./components/footer";
 import Project from "./pages/Project/index";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
