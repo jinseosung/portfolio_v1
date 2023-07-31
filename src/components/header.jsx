12b6f6fb351a08e48eaad8a1e740fccc85b4c45c
Jinseo Sung <jinseosung15@gmail.com>
2023-07-31 17:12:37 +0200

    changed all titles to French

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,36 +1,33 @@
 import { Link } from "react-router-dom";
-import styles from '../utils/styles/header.module.css'
+import styles from "../utils/styles/header.module.css";
 import LogoImg from "../assets/logo.ico";
 
 const Header = () => {
   return (
     <div className={styles.Container}>
-      <Link className={styles.LogoLink} to="/">
-        <img className={styles.LogoImg} src={LogoImg} alt="logo" />
-        <h1 className={styles.Title}>Jinseo</h1>
-      </Link>
-      <nav>
-        <ul className={styles.Menu}>
-        <Link className={`${styles.MenuItem} ${styles.Active}`} to="#about">
-            <li>HOME</li>
-          </Link>
-          <Link className={styles.MenuItem} to="#about">
-            <li>ABOUT</li>
-          </Link>
-          <Link className={styles.MenuItem} to="#skills">
-            <li>SKILLS</li>
-          </Link>
-          <Link className={styles.MenuItem} to="#projects">
-            <li>PROJECTS</li>
-          </Link>
-          <Link className={styles.MenuItem} to="testimonials">
-            <li>TESTIMONIALS</li>
-          </Link>
-          <Link className={styles.MenuItem} to="contact">
-            <li>CONTACT</li>
-          </Link>
-        </ul>
-      </nav>
+        <Link className={styles.LogoLink} to="/">
+          <img className={styles.LogoImg} src={LogoImg} alt="logo" />
+          <h1 className={styles.Title}>Jinseo</h1>
+        </Link>
+        <nav>
+          <ul className={styles.Menu}>
+            <Link className={`${styles.MenuItem} ${styles.Active}`} to="#about">
+              <li>HOME</li>
+            </Link>
+            <Link className={styles.MenuItem} to="#about">
+              <li>A PROPOS</li>
+            </Link>
+            <Link className={styles.MenuItem} to="#projects">
+              <li>PROJETS</li>
+            </Link>
+            <Link className={styles.MenuItem} to="testimonials">
+              <li>TEMOIGNAGES</li>
+            </Link>
+            <Link className={styles.MenuItem} to="#contact">
+              <li>CONTACT</li>
+            </Link>
+          </ul>
+        </nav>
     </div>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import Profile from "../../components/profile";
 import About from "../../components/about";
+import Projects from "../../components/projects";
+import Testimonial from "../../components/testimonial";
 
 const Home = () => {
   return (
     <div>
       <Profile />
       <About />
+      <Projects />
+      <Testimonial />
     </div>
   );
 };
