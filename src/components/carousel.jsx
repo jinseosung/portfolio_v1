159ce615494b2a60416a341a038cee04b87c9dd8
Jinseo Sung <jinseosung15@gmail.com>
2023-08-08 14:56:16 +0200

    add Swiper to Caroussel component

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Modal from "./modal";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,16 +11,20 @@ import "../utils/styles/carousel.css";
 
 import { Navigation, Pagination } from "swiper/modules";
 
-import { Link } from "react-router-dom";
-
 const Carousel = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [projectIdx, setProjectIdx] = useState(0);
+  const toggleModal = () => {
+    !isOpen ? setIsOpen(true) : setIsOpen(false);
+  };
+
   const projects = props.projectlists;
-  return (
+  return isOpen ? (
     <>
       <Swiper
         slidesPerView={3}
-        spaceBetween={50}
-        loop={true}
+        spaceBetween={58}
+        // loop={true}
         navigation={true}
         pagination={{
           clickable: true,
@@ -27,12 +33,57 @@ const Carousel = (props) => {
         className="mySwiper"
       >
         {projects.map((project, idx) => (
-          <SwiperSlide>
-            <Link key={project.id} to={`/projet/${project.id}`}>
+          <SwiperSlide
+            key={project.id}
+            onClick={() => {
+              toggleModal();
+              setProjectIdx(idx);
+            }}
+          >
+            <div>
               <img src={project.image} alt={project.title} />
-              <p className="project__title">{project.title}</p>
-              <p className="project__description">{project.description}</p>
-            </Link>
+              <div className="project__container">
+                <p className="project__title">{project.title}</p>
+                <p className="project__description">{project.description}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Modal
+        toggleModal={toggleModal}
+        projects={projects}
+        projectIdx={projectIdx}
+      />
+    </>
+  ) : (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={58}
+        // loop={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {projects.map((project, idx) => (
+          <SwiperSlide
+            key={project.id}
+            onClick={() => {
+              toggleModal();
+              setProjectIdx(idx);
+            }}
+          >
+            <div>
+              <img src={project.image} alt={project.title} />
+              <div className="project__container">
+                <p className="project__title">{project.title}</p>
+                <p className="project__description">{project.description}</p>
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
