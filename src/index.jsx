5f86f94f27a5ae50dcd2be00b8e6f5fde048a900
Jinseo Sung <jinseosung15@gmail.com>
2023-08-10 10:45:06 +0200

    modify all url of images to project.json

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./utils/styles/style.css";
 import Header from "./components/header";
 import Home from "./pages/Home/index";
@@ -13,9 +13,7 @@ root.render(
     <BrowserRouter>
       <ScrollToTop />
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
