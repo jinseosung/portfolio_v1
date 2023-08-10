722d0a2b906c334e5896e89f59fa1b4335b13da5
Jinseo Sung <jinseosung15@gmail.com>
2023-08-10 08:40:02 +0200

    delete project page

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import "./utils/styles/style.css";
 import Header from "./components/header";
 import Home from "./pages/Home/index";
 import Footer from "./components/footer";
-import Project from "./pages/Project/index";
 import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,7 +15,6 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projet/:projectId" element={<Project />} />
       </Routes>
       <Footer />
     </BrowserRouter>
